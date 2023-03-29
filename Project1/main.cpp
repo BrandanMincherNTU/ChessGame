a5fca6c2893cfb9593a132a3660186938fb724b8
brand <brand@BRANDAN-LAPTOP>
2023-03-29 11:53:19 +0100

    Add project files.

---
+++ Project1/main.cpp
@@ -0,0 +1,30 @@
+
+#include "main.h"
+
+
+
+
+int main()
+{
+	
+	
+	
+	
+	
+	ChessGame ChessWindow;
+	ChessWindow.width = 800;
+	ChessWindow.height = 800;
+	ChessWindow.name = "ChessBoard";
+	ChessWindow.size = 100;
+	
+	ChessWindow.createwindow();
+	
+		
+		
+
+	
+	
+
+}
+
+
---
+++ Project1/main.h
@@ -0,0 +1,689 @@
+#pragma once
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+int board[8][8] =
+{
+	-4,-2,-3,-5,-6,-3,-2,-4,
+	0,-1,-1,-1,-1,-1,-1,-1,
+	0 ,0 ,0 ,0 ,0, 0, 0, 0,
+	-1 ,0 ,0 ,0 ,0, 0, 0, 0,
+	0 ,0 ,0 ,-1 ,0, 0, 0, 0,
+	0 ,0 ,0 ,0 ,0, 0, 0, 0,
+	1, 1, 1, 1, 1, 1, 1, 1,
+	4, 2, 3, 5, 6, 3, 2, 4
+
+};
+
+
+
+struct ChessPiece
+{
+	int x, y;
+	int pieceID;
+	int color;
+	int draw;
+
+};
+
+
+class ChessGame
+{
+private:
+	sf::RenderWindow* window;
+	
+
+
+public:
+	int width;
+	int height;
+	const char* name;
+	int size;
+	
+
+
+
+
+
+	void createwindow()
+	{
+		ChessPiece blackrook1;
+		blackrook1.x = 0;
+		blackrook1.y = 0;
+		blackrook1.pieceID = -4;
+		blackrook1.draw = 1;
+
+
+
+		sf::Texture blackrooktex;
+		blackrooktex.loadFromFile("images/blackrook.png");
+		sf::Sprite blackrooksprite1(blackrooktex);
+		blackrooksprite1.setScale(1.5, 1.5);
+		blackrooksprite1.setPosition(0.f, 0.f);
+
+
+
+
+
+
+
+		sf::RenderWindow window(sf::VideoMode(width, height), name);
+
+
+
+		sf::RectangleShape a1(sf::Vector2f(100, 100));
+		a1.setFillColor(sf::Color(255, 255, 255));
+
+		sf::RectangleShape a2(sf::Vector2f(100, 100));
+		a2.setFillColor(sf::Color(115, 147, 179));
+		a2.setPosition(100.f, 0.f);
+
+		sf::RectangleShape a3(sf::Vector2f(100, 100));
+		a3.setFillColor(sf::Color(255, 255, 255));
+		a3.setPosition(200.f, 0.f);
+
+		sf::RectangleShape a4(sf::Vector2f(100, 100));
+		a4.setFillColor(sf::Color(115, 147, 179));
+		a4.setPosition(300.f, 0.f);
+
+		sf::RectangleShape a5(sf::Vector2f(100, 100));
+		a5.setFillColor(sf::Color(255, 255, 255));
+		a5.setPosition(400.f, 0.f);
+
+		sf::RectangleShape a6(sf::Vector2f(100, 100));
+		a6.setFillColor(sf::Color(115, 147, 179));
+		a6.setPosition(500.f, 0.f);
+
+		sf::RectangleShape a7(sf::Vector2f(100, 100));
+		a7.setFillColor(sf::Color(255, 255, 255));
+		a7.setPosition(600.f, 0.f);
+
+		sf::RectangleShape a8(sf::Vector2f(100, 100));
+		a8.setFillColor(sf::Color(115, 147, 179));
+		a8.setPosition(700.f, 0.f);
+
+
+
+		sf::RectangleShape b1(sf::Vector2f(100, 100));
+		b1.setFillColor(sf::Color(115, 147, 179));
+		b1.setPosition(0.f, 100.f);
+
+		sf::RectangleShape b2(sf::Vector2f(100, 100));
+		b2.setFillColor(sf::Color(255, 255, 255));
+		b2.setPosition(100.f, 100.f);
+
+		sf::RectangleShape b3(sf::Vector2f(100, 100));
+		b3.setFillColor(sf::Color(115, 147, 179));
+		b3.setPosition(200.f, 100.f);
+
+		sf::RectangleShape b4(sf::Vector2f(100, 100));
+		b4.setFillColor(sf::Color(255, 255, 255));
+		b4.setPosition(300.f, 100.f);
+
+		sf::RectangleShape b5(sf::Vector2f(100, 100));
+		b5.setFillColor(sf::Color(115, 147, 179));
+		b5.setPosition(400.f, 100.f);
+
+		sf::RectangleShape b6(sf::Vector2f(100, 100));
+		b6.setFillColor(sf::Color(255, 255, 255));
+		b6.setPosition(500.f, 100.f);
+
+		sf::RectangleShape b7(sf::Vector2f(100, 100));
+		b7.setFillColor(sf::Color(115, 147, 179));
+		b7.setPosition(600.f, 100.f);
+
+		sf::RectangleShape b8(sf::Vector2f(100, 100));
+		b8.setFillColor(sf::Color(255, 255, 255));
+		b8.setPosition(700.f, 100.f);
+
+		sf::RectangleShape c1(sf::Vector2f(100, 100));
+		c1.setFillColor(sf::Color(255, 255, 255));
+		c1.setPosition(0.f, 200.f);
+
+		sf::RectangleShape c2(sf::Vector2f(100, 100));
+		c2.setFillColor(sf::Color(115, 147, 179));
+		c2.setPosition(100.f, 200.f);
+
+		sf::RectangleShape c3(sf::Vector2f(100, 100));
+		c3.setFillColor(sf::Color(255, 255, 255));
+		c3.setPosition(200.f, 200.f);
+
+		sf::RectangleShape c4(sf::Vector2f(100, 100));
+		c4.setFillColor(sf::Color(115, 147, 179));
+		c4.setPosition(300.f, 200.f);
+
+		sf::RectangleShape c5(sf::Vector2f(100, 100));
+		c5.setFillColor(sf::Color(255, 255, 255));
+		c5.setPosition(400.f, 200.f);
+
+		sf::RectangleShape c6(sf::Vector2f(100, 100));
+		c6.setFillColor(sf::Color(115, 147, 179));
+		c6.setPosition(500.f, 200.f);
+
+		sf::RectangleShape c7(sf::Vector2f(100, 100));
+		c7.setFillColor(sf::Color(255, 255, 255));
+		c7.setPosition(600.f, 200.f);
+
+		sf::RectangleShape c8(sf::Vector2f(100, 100));
+		c8.setFillColor(sf::Color(115, 147, 179));
+		c8.setPosition(700.f, 200.f);
+
+		sf::RectangleShape d1(sf::Vector2f(100, 100));
+		d1.setFillColor(sf::Color(115, 147, 179));
+		d1.setPosition(0.f, 300.f);
+
+		sf::RectangleShape d2(sf::Vector2f(100, 100));
+		d2.setFillColor(sf::Color(255, 255, 255));
+		d2.setPosition(100.f, 300.f);
+
+		sf::RectangleShape d3(sf::Vector2f(100, 100));
+		d3.setFillColor(sf::Color(115, 147, 179));
+		d3.setPosition(200.f, 300.f);
+
+		sf::RectangleShape d4(sf::Vector2f(100, 100));
+		d4.setFillColor(sf::Color(255, 255, 255));
+		d4.setPosition(300.f, 300.f);
+
+		sf::RectangleShape d5(sf::Vector2f(100, 100));
+		d5.setFillColor(sf::Color(115, 147, 179));
+		d5.setPosition(400.f, 300.f);
+
+		sf::RectangleShape d6(sf::Vector2f(100, 100));
+		d6.setFillColor(sf::Color(255, 255, 255));
+		d6.setPosition(500.f, 300.f);
+
+		sf::RectangleShape d7(sf::Vector2f(100, 100));
+		d7.setFillColor(sf::Color(115, 147, 179));
+		d7.setPosition(600.f, 300.f);
+
+		sf::RectangleShape d8(sf::Vector2f(100, 100));
+		d8.setFillColor(sf::Color(255, 255, 255));
+		d8.setPosition(700.f, 300.f);
+
+		sf::RectangleShape e1(sf::Vector2f(100, 100));
+		e1.setFillColor(sf::Color(255, 255, 255));
+		e1.setPosition(0.f, 400.f);
+
+		sf::RectangleShape e2(sf::Vector2f(100, 100));
+		e2.setFillColor(sf::Color(115, 147, 179));
+		e2.setPosition(100.f, 400.f);
+
+		sf::RectangleShape e3(sf::Vector2f(100, 100));
+		e3.setFillColor(sf::Color(255, 255, 255));
+		e3.setPosition(200.f, 400.f);
+
+		sf::RectangleShape e4(sf::Vector2f(100, 100));
+		e4.setFillColor(sf::Color(115, 147, 179));
+		e4.setPosition(300.f, 400.f);
+
+		sf::RectangleShape e5(sf::Vector2f(100, 100));
+		e5.setFillColor(sf::Color(255, 255, 255));
+		e5.setPosition(400.f, 400.f);
+
+		sf::RectangleShape e6(sf::Vector2f(100, 100));
+		e6.setFillColor(sf::Color(115, 147, 179));
+		e6.setPosition(500.f, 400.f);
+
+		sf::RectangleShape e7(sf::Vector2f(100, 100));
+		e7.setFillColor(sf::Color(255, 255, 255));
+		e7.setPosition(600.f, 400.f);
+
+		sf::RectangleShape e8(sf::Vector2f(100, 100));
+		e8.setFillColor(sf::Color(115, 147, 179));
+		e8.setPosition(700.f, 400.f);
+
+		sf::RectangleShape f1(sf::Vector2f(100, 100));
+		f1.setFillColor(sf::Color(115, 147, 179));
+		f1.setPosition(0.f, 500.f);
+
+		sf::RectangleShape f2(sf::Vector2f(100, 100));
+		f2.setFillColor(sf::Color(255, 255, 255));
+		f2.setPosition(100.f, 500.f);
+
+		sf::RectangleShape f3(sf::Vector2f(100, 100));
+		f3.setFillColor(sf::Color(115, 147, 179));
+		f3.setPosition(200.f, 500.f);
+
+		sf::RectangleShape f4(sf::Vector2f(100, 100));
+		f4.setFillColor(sf::Color(255, 255, 255));
+		f4.setPosition(300.f, 500.f);
+
+		sf::RectangleShape f5(sf::Vector2f(100, 100));
+		f5.setFillColor(sf::Color(115, 147, 179));
+		f5.setPosition(400.f, 500.f);
+
+		sf::RectangleShape f6(sf::Vector2f(100, 100));
+		f6.setFillColor(sf::Color(255, 255, 255));
+		f6.setPosition(500.f, 500.f);
+
+		sf::RectangleShape f7(sf::Vector2f(100, 100));
+		f7.setFillColor(sf::Color(115, 147, 179));
+		f7.setPosition(600.f, 500.f);
+
+		sf::RectangleShape f8(sf::Vector2f(100, 100));
+		f8.setFillColor(sf::Color(255, 255, 255));
+		f8.setPosition(700.f, 500.f);
+
+		sf::RectangleShape g1(sf::Vector2f(100, 100));
+		g1.setFillColor(sf::Color(255, 255, 255));
+		g1.setPosition(0.f, 600.f);
+
+		sf::RectangleShape g2(sf::Vector2f(100, 100));
+		g2.setFillColor(sf::Color(115, 147, 179));
+		g2.setPosition(100.f, 600.f);
+
+		sf::RectangleShape g3(sf::Vector2f(100, 100));
+		g3.setFillColor(sf::Color(255, 255, 255));
+		g3.setPosition(200.f, 600.f);
+
+		sf::RectangleShape g4(sf::Vector2f(100, 100));
+		g4.setFillColor(sf::Color(115, 147, 179));
+		g4.setPosition(300.f, 600.f);
+
+		sf::RectangleShape g5(sf::Vector2f(100, 100));
+		g5.setFillColor(sf::Color(255, 255, 255));
+		g5.setPosition(400.f, 600.f);
+
+		sf::RectangleShape g6(sf::Vector2f(100, 100));
+		g6.setFillColor(sf::Color(115, 147, 179));
+		g6.setPosition(500.f, 600.f);
+
+		sf::RectangleShape g7(sf::Vector2f(100, 100));
+		g7.setFillColor(sf::Color(255, 255, 255));
+		g7.setPosition(600.f, 600.f);
+
+		sf::RectangleShape g8(sf::Vector2f(100, 100));
+		g8.setFillColor(sf::Color(115, 147, 179));
+		g8.setPosition(700.f, 600.f);
+
+		sf::RectangleShape h1(sf::Vector2f(100, 100));
+		h1.setFillColor(sf::Color(115, 147, 179));
+		h1.setPosition(0.f, 700.f);
+
+		sf::RectangleShape h2(sf::Vector2f(100, 100));
+		h2.setFillColor(sf::Color(255, 255, 255));
+		h2.setPosition(100.f, 700.f);
+
+		sf::RectangleShape h3(sf::Vector2f(100, 100));
+		h3.setFillColor(sf::Color(115, 147, 179));
+		h3.setPosition(200.f, 700.f);
+
+		sf::RectangleShape h4(sf::Vector2f(100, 100));
+		h4.setFillColor(sf::Color(255, 255, 255));
+		h4.setPosition(300.f, 700.f);
+
+		sf::RectangleShape h5(sf::Vector2f(100, 100));
+		h5.setFillColor(sf::Color(115, 147, 179));
+		h5.setPosition(400.f, 700.f);
+
+		sf::RectangleShape h6(sf::Vector2f(100, 100));
+		h6.setFillColor(sf::Color(255, 255, 255));
+		h6.setPosition(500.f, 700.f);
+
+		sf::RectangleShape h7(sf::Vector2f(100, 100));
+		h7.setFillColor(sf::Color(115, 147, 179));
+		h7.setPosition(600.f, 700.f);
+
+		sf::RectangleShape h8(sf::Vector2f(100, 100));
+		h8.setFillColor(sf::Color(255, 255, 255));
+		h8.setPosition(700.f, 700.f);
+
+
+		float dx = 0, dy = 0;
+
+
+		bool a1square = 0;
+
+		int moving = 0;
+		int valid = 0;
+
+		int oldx, oldy;
+
+		while (window.isOpen())
+		{
+			sf::Event event;
+			sf::Event MouseReleased;
+			while (window.pollEvent(event))
+			{
+				if (event.type == sf::Event::Closed)
+				{
+
+					window.close();
+				}
+				window.clear();
+			}
+			if (event.type == sf::Event::MouseButtonPressed)
+			{
+				if (event.key.code == sf::Mouse::Left)
+
+
+
+				{
+					sf::Vector2i mpos = sf::Mouse::getPosition(window);
+					int x = mpos.x / size;
+					int y = mpos.y / size;
+					valid = 0;
+					// std::cout << x << std::endl;
+					//std::cout << y << std::endl;
+					std::cout << "ChessPiece: " << board[y][x] << "   ";
+					std::cout << "x: " << x << "   " << "y: " << y << std::endl;
+					if (board[y][x] != 0)
+					{
+
+
+						// std::cout << dx <<"   " << dy << std::endl;
+						
+						
+
+					}
+
+
+					if (board[y][x] == blackrook1.pieceID)
+					{
+						oldx = x;
+						oldy = y;
+
+
+						std::cout << "Moving Rook";
+						moving = blackrook1.pieceID;
+
+					}
+
+
+				}
+				if (event.type == sf::Event::MouseButtonPressed)
+				{
+					if (event.key.code == sf::Mouse::Middle)
+					{
+						std::cout << "cancelled";
+						moving = 0;
+
+					}
+
+				}
+
+				if (moving == blackrook1.pieceID && event.type == sf::Event::MouseButtonPressed)
+				{
+					if (event.key.code == sf::Mouse::Right)
+					{
+
+
+
+						sf::Vector2i mpos = sf::Mouse::getPosition(window);
+						int x = mpos.x / size;
+						int y = mpos.y / size;
+						int dx = x - oldx;
+						int dy = y - oldy;
+						int checker = 0;
+
+						for (int i = 0; i < 8; i++)
+						{
+
+
+
+
+							if ((x == oldx + i && y == oldy || x == oldx - i && y == oldy) || (y == oldy + i && x == oldx || y == oldy - i && x == oldx))
+							{
+
+								if (oldy  < y && x == oldx) // Rook moving down
+								{
+									
+									for (int i = oldy + 1; i <= y; i++)
+									{
+										if (board[y][x] >= 0 && board[i][x] >= 0 || board[i][x] == -4)
+										{
+											checker++;
+
+										}
+
+
+									}
+									
+									if (board[y][x] >= 0 && checker == y - oldy)
+
+
+									{
+										valid = 1;
+
+										blackrooksprite1.setPosition(x * size, y * size);
+
+										board[y][x] = -4;
+										board[oldy][oldx] = 0;
+										moving = 0;
+
+										std::cout << "///MOVED//" << "ChessPiece: " << board[y][x] << "   " << std::endl;
+										std::cout << "chcker: " << checker << std::endl;
+										std::cout << "oldx: " << oldx << "   " << "oldy: " << oldy << std::endl;
+										std::cout << "TO: " << "x: " << x << "   " << "y: " << y << std::endl;
+										checker = 0;
+
+									}
+
+
+
+								
+
+
+
+
+								}
+
+								if (oldy > y && x == oldx) // Rook Moving Up
+								{
+									for (int i = oldy - 1; i >= y; i--)
+									{
+										if (board[y][x] >= 0 && board[i][x] >= 0 || board[i][x] == -4)
+										{
+											checker++;
+
+										}
+
+
+									}
+									
+									
+									if (board[y][x] >= 0 && checker == oldy-y)
+									{
+										valid = 1;
+
+
+										blackrooksprite1.setPosition(x * size, y * size);
+
+										board[y][x] = -4;
+										board[oldy][oldx] = 0;
+										moving = 0;
+
+										std::cout << "///MOVED//" << "ChessPiece: " << board[y][x] << "   " << std::endl;
+
+										std::cout << "oldx: " << oldx << "   " << "oldy: " << oldy << std::endl;
+										std::cout << "TO: " << "x: " << x << "   " << "y: " << y << std::endl;
+
+
+									}
+
+
+								}
+
+
+
+								if (oldx < x && y == oldy) // Rook Moving Right
+								{
+									
+									for (int i = oldx + 1; i <= x; i++)
+									{
+										if (board[y][x] >= 0 && board[y][i] >= 0 || board[y][i] == -4)
+										{
+											checker++;
+
+										}
+									}
+									
+									if (board[y][x] >= 0 && checker == x - oldx)
+									{
+										valid = 1;
+
+
+										blackrooksprite1.setPosition(x * size, y * size);
+
+										board[y][x] = -4;
+										board[oldy][oldx] = 0;
+										moving = 0;
+
+										std::cout << "///MOVED//" << "ChessPiece: " << board[y][x] << "   " << std::endl;
+
+										std::cout << "oldx: " << oldx << "   " << "oldy: " << oldy << std::endl;
+										std::cout << "TO: " << "x: " << x << "   " << "y: " << y << std::endl;
+									}
+
+
+
+								}
+
+								if (oldx > x && y == oldy) // left
+								{
+
+									for (int i = oldx - 1; i >= x; i--)
+									{
+										if (board[y][x] >= 0 && board[y][i] >= 0 || board[y][i] == -4)
+										{
+											checker++;
+
+										}
+
+
+									}
+
+									if(board[y][x] >= 0 && checker == oldx - x)
+									{
+										valid = 1;
+
+
+										blackrooksprite1.setPosition(x * size, y * size);
+
+										board[y][x] = -4;
+										board[oldy][oldx] = 0;
+										moving = 0;
+
+										std::cout << "///MOVED//" << "ChessPiece: " << board[y][x] << "   " << std::endl;
+
+										std::cout << "oldx: " << oldx << "   " << "oldy: " << oldy << std::endl;
+										std::cout << "TO: " << "x: " << x << "   " << "y: " << y << std::endl;
+									}
+
+								}
+
+								if (valid == 0)
+								{
+									std::cout << "chcker: " << checker << std::endl;
+									std::cout << "illegal Move" << std::endl;
+								}
+
+								
+								
+
+
+							}
+						}
+
+
+					}
+
+
+				}
+
+			}
+
+			window.draw(a1);
+			window.draw(a2);
+			window.draw(a3);
+			window.draw(a4);
+			window.draw(a5);
+			window.draw(a6);
+			window.draw(a7);
+			window.draw(a8);
+
+			window.draw(b1);
+			window.draw(b2);
+			window.draw(b3);
+			window.draw(b4);
+			window.draw(b5);
+			window.draw(b6);
+			window.draw(b7);
+			window.draw(b8);
+
+			window.draw(c1);
+			window.draw(c2);
+			window.draw(c3);
+			window.draw(c4);
+			window.draw(c5);
+			window.draw(c6);
+			window.draw(c7);
+			window.draw(c8);
+
+			window.draw(d1);
+			window.draw(d2);
+			window.draw(d3);
+			window.draw(d4);
+			window.draw(d5);
+			window.draw(d6);
+			window.draw(d7);
+			window.draw(d8);
+
+			window.draw(e1);
+			window.draw(e2);
+			window.draw(e3);
+			window.draw(e4);
+			window.draw(e5);
+			window.draw(e6);
+			window.draw(e7);
+			window.draw(e8);
+
+			window.draw(f1);
+			window.draw(f2);
+			window.draw(f3);
+			window.draw(f4);
+			window.draw(f5);
+			window.draw(f6);
+			window.draw(f7);
+			window.draw(f8);
+
+			window.draw(g1);
+			window.draw(g2);
+			window.draw(g3);
+			window.draw(g4);
+			window.draw(g5);
+			window.draw(g6);
+			window.draw(g7);
+			window.draw(g8);
+
+			window.draw(h1);
+			window.draw(h2);
+			window.draw(h3);
+			window.draw(h4);
+			window.draw(h5);
+			window.draw(h6);
+			window.draw(h7);
+			window.draw(h8);
+
+
+			if (blackrook1.draw == 1)
+			{
+				window.draw(blackrooksprite1);
+
+			}
+
+
+
+			window.display();
+
+
+		}
+	}
+
+
+};
